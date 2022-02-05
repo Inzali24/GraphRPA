57b5c331df5d7a02b3f4d07abe47895241522da9
inzalinaing <inzalinaing24@gmail.com>
2022-02-05 20:48:04 +0630

    git test2 add

--- pages/stackedbar.js
+++ pages/stackedbar.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Chart } from 'react-chartjs-2';
 import {Line} from 'react-chartjs-2';
 
 const DATA_COUNT = 7;
-const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
 
 const labels = [
     'January',
@@ -18,7 +16,7 @@ const data = {
   labels: labels,
   datasets: [
     {
-      label: 'Dataset 1',
+      label: 'Number of Submissions',
       data: [0, 10, 5, 2, 20, 30, 45],
       backgroundColor: 'rgba(75,192,192,0.4)',
       borderColor: 'rgba(75,192,192,1)',
@@ -26,16 +24,16 @@ const data = {
       type: 'bar'
     },
     {
-      label: 'Dataset 2',
+      label: 'Correct Rate',
       data: [0, 10, 5, 2, 20, 30, 45],
       backgroundColor: 'rgba(255,99,132,0.2)',
-		borderColor: 'rgba(255,99,132,1)',
+		  borderColor: 'rgba(255,99,132,1)',
       stack: 'combined'
     }
   ]
 };
 
-export default () => (
+export default () => (  
     <div>
       <h2>Stackedbar Example wahaha</h2>
       <Line
